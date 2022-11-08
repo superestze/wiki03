a6664b684346b538fa9b0ef968cf64608c563835
zehui <sbura@sina.com>
2022-11-08 17:01:32 +0800

    2-2 init

---
+++ src/main/java/com/zhengqi/wiki03/Wiki03Application.java
@@ -0,0 +1,13 @@
+package com.zhengqi.wiki03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wiki03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wiki03Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhengqi/wiki03/Wiki03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhengqi.wiki03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wiki03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
