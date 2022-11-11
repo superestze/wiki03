550afdf0aae7f9fb57c6e78221f29dee990ed19e
zehui <sbura@sina.com>
2022-11-11 16:24:05 +0800

    3-6 增加通用的返回类commonresp

--- src/main/java/com/zhengqi/wiki03/controller/EbookController.java
+++ src/main/java/com/zhengqi/wiki03/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.zhengqi.wiki03.controller;
 
 import com.zhengqi.wiki03.domain.Ebook;
+import com.zhengqi.wiki03.resp.CommonResp;
 import com.zhengqi.wiki03.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,10 @@ public class EbookController {
      * @return
      */
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp<List<Ebook>> list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<List<Ebook>>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
