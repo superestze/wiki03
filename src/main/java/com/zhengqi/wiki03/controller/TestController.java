ae22acbea200074f935505624efef763667fe09c
zehui <sbura@sina.com>
2022-11-17 11:48:09 +0800

    9-8 增加redis 测试接口

--- src/main/java/com/zhengqi/wiki03/controller/TestController.java
+++ src/main/java/com/zhengqi/wiki03/controller/TestController.java
@@ -2,7 +2,12 @@ package com.zhengqi.wiki03.controller;
 
 import com.zhengqi.wiki03.domain.Test;
 import com.zhengqi.wiki03.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -14,6 +19,11 @@ public class TestController {
     @Resource
     private TestService testService;
 
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    private final static Logger LOG = LoggerFactory.getLogger(TestController.class);
+
     /**
      * GET POST PUT DELETE
      *
@@ -22,5 +32,12 @@ public class TestController {
     @GetMapping("/list")
     public List<Test> list() {
         return testService.list();
+    }
+
+    @GetMapping("/redis/get/{key}")
+    public Object getRedisKey(@PathVariable Long key) {
+        Object object = redisTemplate.opsForValue().get(key);
+        LOG.info("key:{}, value: {}", key, object);
+        return object;
     }
 }
