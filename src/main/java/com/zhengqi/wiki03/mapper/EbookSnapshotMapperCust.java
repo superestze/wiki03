656cbd705b94d2621c54fd6eb21bbbf73c893e3f
zehui <sbura@sina.com>
2022-11-19 01:13:18 +0800

    11-9 增加获取最近三十天的阅读和点赞增量

--- src/main/java/com/zhengqi/wiki03/mapper/EbookSnapshotMapperCust.java
+++ src/main/java/com/zhengqi/wiki03/mapper/EbookSnapshotMapperCust.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface EbookSnapshotMapperCust {
     public void genSnapshot();
     public List<StatisticResp> getStatistic();
+
+    List<StatisticResp> get30Statistic();
 }
